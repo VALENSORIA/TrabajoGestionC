f0e1b7cfef235b72b59d66facd4ee39cc50b7003
valensoria <valentin.soria.epet20@gmail.com>
2023-05-23 00:12:17 -0300

    Copiando primeras lineas del anterior js

--- Desencript.js
+++ Desencript.js
@@ -1,2 +1,18 @@
-ss
+
+function desencriptarNumero(event) {
+    event.preventDefault(); // Evitar el envío del formulario
   
+    // Obtener el valor del número encriptado ingresado
+    var numeroEncriptado = document.getElementById('numeroEncriptado').value;
+  
+    // Verificar si el número tiene 4 dígitos
+    if (numeroEncriptado.length !== 4) {
+      alert("Por favor, ingrese un número encriptado de 4 dígitos.");
+      return;
+    }
+  
+    // Convertir el número encriptado en un arreglo de dígitos
+    var digitos = numeroEncriptado.split("");
+  
+    
+  }
