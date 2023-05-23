3eb967c86b83b87499f86d21542e2790b96aee64
valensoria <valentin.soria.epet20@gmail.com>
2023-05-23 00:14:43 -0300

    Deshaciendo encriptacion por digito

--- Desencript.js
+++ Desencript.js
@@ -24,6 +24,19 @@ function desencriptarNumero(event) {
     digitos[1] = digitos[3];
     digitos[3] = temp;
   
+    // Deshacer la encriptación aplicada a cada dígito
+    for (var i = 0; i < digitos.length; i++) {
+      var digito = parseInt(digitos[i]);
+  
+      // Deshacer el reemplazo del dígito con el residuo de la división entre 10 de la suma de dicho dígito más 7
+      digito = digito - 7;
+      if (digito < 0) {
+        digito = digito + 10; // Asegurar que el resultado sea un dígito válido (positivo)
+      }
+  
+      // Actualizar el dígito en la matriz de dígitos
+      digitos[i] = digito.toString();
+    }
   
     
   }
