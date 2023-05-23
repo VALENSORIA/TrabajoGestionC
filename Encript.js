5d8fd2b0b0d92ac179859d461c19e5897431ecf0
valensoria <valentin.soria.epet20@gmail.com>
2023-05-22 23:49:40 -0300

    Condicional para verificar que tenga 4 digitos

--- Encript.js
+++ Encript.js
@@ -1,4 +1,15 @@
 function encriptarNumero(event) {
-    event.preventDefault();
+    event.preventDefault(); // Evitar el envío del formulario
+  
+    // Obtener el valor del número ingresado
+    var numero = document.getElementById('numero').value;
+  
+    // Verificar si el número tiene 4 dígitos
+    if (numero.length !== 4) {
+      alert("Por favor, ingrese un número de 4 dígitos.");
+      return;
+    }
+  
+    
   }
   
