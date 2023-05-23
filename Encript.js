b199f799ce402a2575d73a488033be9f496fb4d6
valensoria <valentin.soria.epet20@gmail.com>
2023-05-22 23:48:37 -0300

    Agregando linea para evitar el envio erroneo

--- Encript.js
+++ Encript.js
@@ -1,10 +1,4 @@
-function guardarNumero(event) {
-    event.preventDefault(); // Evitar el envío del formulario
-  
-    // Obtener el valor del número ingresado
-    var numero = document.getElementById('numero').value;
-  
-    // Mostrar el número en el elemento de resultado
-    document.getElementById('resultado').innerHTML = "El número ingresado es: " + numero;
+function encriptarNumero(event) {
+    event.preventDefault();
   }
   
